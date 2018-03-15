566977d7027c5403cb4958f4d36f507c4b3c1f12
Brian Soumakian <bsoumakian@gmail.com>
2018-03-15 16:27:44 -0700

    add types for typescript

---
+++ index.d.ts
@@ -0,0 +1,57 @@
+export = PersonalityTextSummary;
+
+declare type Locale = 'en' | 'es' | 'ja' | 'ko';
+declare type Version = 'v2' | 'v3';
+declare type Format = 'plain' | 'html' | 'markdown';
+
+declare class PersonalityTextSummary {
+  constructor(options?: {
+    locale?: Locale,
+    version?: Version,
+    format?: Format
+  });
+
+  defaultOptions(): {
+    locale: Locale,
+    version: Version
+  };
+
+  getSummary(profile: any): string;
+
+  assemble(profile: any): string[][];
+
+  assembleTraits(traits: any): string[];
+
+  assembleFacets(traits: any): string[];
+
+  assembleNeeds(needs: any): string[];
+
+  assembleValues(values: any): string[];
+
+  getCircumplexAdjective(p1: any, p2: any, order: any): string;
+
+  getValueInfo(value: any): {
+    name: string,
+    term: string,
+    description: string
+  };
+
+  getFacetInfo(facet: any): {
+    name: string,
+    term: string,
+    description: string
+  };
+
+  getFacet(id: any): {
+    LowTerm: string,
+    LowDescription: string,
+    HighTerm: string,
+    HighDescription: string,
+    Big5: string
+  };
+
+  getTrait(id: any): {
+    HighDescription: string,
+    LowDescription: string
+  };
+}
